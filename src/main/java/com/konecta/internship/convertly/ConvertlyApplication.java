6d8b348676bfe2fc44998586d44f3fc51bf89775
Mohanad Ahmed <mhndahmd031@gmail.com>
2025-08-05 18:51:55 +0300

    init commit

---
+++ src/main/java/com/konecta/internship/convertly/ConvertlyApplication.java
@@ -0,0 +1,13 @@
+package com.konecta.internship.convertly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konecta/internship/convertly/ConvertlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konecta.internship.convertly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
